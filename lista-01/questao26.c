7dd6a8df744d8a78f299b4205a13c885655029b1
BiaD-Cam <bia.camara72@gmail.com>
2026-08-26 23:26:45 +0000

    Correção

--- lista-01/questao26.c
+++ lista-01/questao26.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-    printf("       *       \n");
-    printf("      / \\      \n");
-    printf("     / o \\     \n");
-    printf("    / o o \\    \n");
-    printf("   / o o o \\   \n");
-    printf("  / o o o o \\  \n");
-    printf(" / o o o o o \\ \n");
-    printf("--------------- \n");
+    printf("        *       \n");
+    printf("      X ºX      \n");
+    printf("     Xº x XX     \n");
+    printf("    X xº x XX    \n");
+    printf("   Xº x xº x XX   \n");
+    printf("  X x xº xº x XXX  \n");
+    printf(" X xº x x xº x XXX \n");
+    printf("xxxxxxxxxxxxxxxxxxx \n");
     printf("      | |      \n");
     printf("      | |      \n");
     return 0;
